30248b19f76d035fe8a346d5c02a331f6ba8fb16
AL11J112 <eduar_csc_@hotmail.com>
2013-04-08 13:44:21 -0500

    Update acciones.js

--- js/acciones.js
+++ js/acciones.js
@@ -30,20 +30,29 @@ $(document).ready(function(e) {
 		   $('#derecha').swiperight(function(){
 			   navigator.notification.alert("deslizo a la derecha",function(){},"practica 1","aceptar");
 		   });
-		   $('#izquierda').swipeleft(function(){
-		   	navigator.notification.alert("¿Que desea hacer?",function(option));{
+		   
+		   	
+	  
+ $('#izquierda').swipeleft(function(){
+		   	navigator.notification.alert("¿Que desea hacer?",function(option){
 		   		switch(option){
 		   			case 1:
 		   			navigator.notification.beep(1);
 		   			break;
-		   			case
-		   		case 2:
+					
+		   		    case 2:
 		   			navigator.notification.vibrate(500);
 		   			break;
 		   		}//switch
 		   	},"practica1","beep,vibrar,cancelar");//function confirm
 		   });//function swipe
-		   	
+
 	  
    },false);
+   
+$('table td').eq(1).text(device.name);
+$('table td').eq(3).text(device.cordova);
+$('table td').eq(5).text(device.platform);
+$('table td').eq(3).text(device.version);
+$('table td').eq(3).text(device.UUID);
 });
